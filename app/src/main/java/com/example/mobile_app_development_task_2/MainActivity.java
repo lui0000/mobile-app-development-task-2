c8b0bf506789eb2b25e9bd84e0e70121cee6e775
Lisa <elbondarenko04@gmail.com>
2024-02-21 11:19:47 +0300

    last commit

--- app/src/main/java/com/example/mobile_app_development_task_2/MainActivity.java
+++ app/src/main/java/com/example/mobile_app_development_task_2/MainActivity.java
@@ -3,18 +3,15 @@ package com.example.mobile_app_development_task_2;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.ActionBar;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setUpActionBar();
-
+    private void showToast(String activityCreated) {
+        String message = "my massage";
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show(); // Метод для отображения сообщения
     }
     private void setUpActionBar() {
         ActionBar actionBar = getSupportActionBar();
@@ -22,5 +19,51 @@ public class MainActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUpActionBar();
+        showToast("Activity created"); // Показываем сообщение при создании активности
+        Log.d("messagee", "OnCreate");
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        showToast("Activity started");
+        Log.d("messagee", "OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showToast("Activity resumed");
+        Log.d("messagee", "OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        showToast("Activity paused");
+        Log.d("messagee", "OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        showToast("Activity stopped");
+        Log.d("messagee", "OnStop");
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d("messagee", "onSaveInstanceState");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        showToast("Activity destroyed");
+        Log.d("messagee", "OnDestroy");
+    }
 }
